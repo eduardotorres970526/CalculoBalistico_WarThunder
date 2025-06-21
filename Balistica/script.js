97728f14f12175f5846961d148c33d2ff47c727a
eduardo_torres <teduardofelipe@gmail.com>
2025-06-20 21:58:12 -0500

    Calculo_correcto

---
+++ Balistica/script.js
@@ -0,0 +1,19 @@
+
+
+// Función para calcular la distancia optima de tiro para el BMP3
+
+function calculoBalistico (cuadranteCm, cuadranteM, distanciaMedidaCM) {
+
+    let metrosaCm = cuadranteM*100
+    let escala = metrosaCm/cuadranteCm
+
+    let distanciaCalculadaCM = distanciaMedidaCM*escala
+
+    let distanciaCalculadaM = distanciaCalculadaCM/100
+     
+    console.log(distanciaCalculadaM)
+
+}
+
+
+calculoBalistico(4.49, 225, 8.98)
